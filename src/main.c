432530bf12323dc2e69e1c47e71a031c112857bb
Lucas A Melo <arcoverdemlucas@gmail.com>
2026-01-16 09:51:11 -0300

    Make accept update contact flags.

--- src/main.c
+++ src/main.c
@@ -30,12 +30,25 @@ int main(int argc, char *argv[])
 
     else if (strcmp(flag, "--view") == 0 || strcmp(flag, "-v") == 0)
     {
+        read_contacts();
+    }
+
+    else if (strcmp(flag, "--update") == 0 || strcmp(flag, "-u") == 0)
+    {
         if (argc <= 2)
         {
             return 1;
         }
 
-        read_contacts();
+        char * contact_index_input = argv[2];
+        int contact_index = atoi(contact_index_input);
+
+        char * name = argv[3];
+        char * phone = argv[4];
+        char * email = argv[5];
+
+        update_contact(contact_index, name, phone, email);
+
     }
 
     return 0;
