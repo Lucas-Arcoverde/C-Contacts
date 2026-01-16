b0f0b62a647fda56db761637a93bbaacbc4538f1
Lucas A Melo <arcoverdemlucas@gmail.com>
2026-01-16 10:04:26 -0300

    Update help message.

--- src/utils.c
+++ src/utils.c
@@ -3,13 +3,25 @@
 
 void display_help_message(char program_name[])
 {
-    printf("\nC-Contacts\n");
-    printf("\nUsage:\n");
-    printf("%s <flag> <arguments>\n\n", program_name);
-    printf("%s -h  --help                        Show this help message.\n\n", program_name);
-    printf("%s -a  --add                         Add a new contact to your list.\n", program_name);
-    printf("... <name> <phone> <email>\n\n");
-    printf("%s -d  --delete                      Delete a contact from your list.\n", program_name);
-    printf("... <contact index>\n\n");
+    printf("\nC-Contacts Manager\n");
+    printf("A simple contact management system\n\n");
+    
+    printf("Usage: %s [COMMAND] [ARGUMENTS]\n\n", program_name);
+    
+    printf("Commands:\n");
+    printf("  -h, --help                    Display this help message\n");
+    printf("  -v, --view                    View all contacts\n");
+    printf("  -a, --add <name> <phone> <email>\n");
+    printf("                                Add a new contact\n");
+    printf("  -u, --update <id> <name> <phone> <email>\n");
+    printf("                                Update an existing contact\n");
+    printf("  -r, --remove <id>            Remove a contact\n");
+    
+    printf("\nExamples:\n");
+    printf("  %s --view\n", program_name);
+    printf("  %s --add \"John\" \"123456\" \"john@mail.com\"\n", program_name);
+    printf("  %s --update 0 \"John Doe\" \"5551234\" \"new@mail.com\"\n", program_name);
+    printf("  %s --remove 2\n\n", program_name);
+    
+    printf("Note: Use quotes for values containing spaces\n");
 }
-
